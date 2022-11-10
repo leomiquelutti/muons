e62486f23c4c2d13d0ce643beed77ecdf7beedb7
Leonardo Miquelutti <lmiquelutti@id.uff.br>
2022-11-10 08:57:47 -0300

    fixed constants.py + execution time measurement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numeric_model
 import logging as log
-
+import time
 
 def main():
     if __name__ == '__main__':
@@ -8,4 +8,6 @@ def main():
         mod = numeric_model.NumericModel("resources/plot.dat", low_x=80, low_y=50, max_x=280, max_y=200, cut=True)
         mod.start_model_processing()
 
+start_time = time.time()
 main()
+print("--- %s seconds ---" % (time.time() - start_time))
